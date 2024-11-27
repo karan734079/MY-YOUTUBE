451e96ae971c387e86112ca6d32c77690bceb373
karan kumar <karanjangral60@gmail.com>
2024-11-27 10:21:06 +0530

    m

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -47,19 +47,22 @@ const WatchPage = () => {
               allowFullScreen
             ></iframe>
             <p className='text-4xl font-semibold mt-1'>{title}</p>
-            <div className='flex mt-1 space-x-1'>
-              <img src={thumbnail} alt="" className='rounded-full h-8 w-8' />
-              <p className='text-lg'>{channelTitle}</p>
-            </div>
+            <p className="text-gray-600 text-base mt-3 ml-1">
+              {formatViews(videoViews)} views
+            </p>
             <div className='flex justify-between'>
-              <p className="text-gray-500 text-sm mt-2 ml-1">
-                {formatViews(videoViews)} views
-              </p>
-              <button className='flex mr-3 gap-2 p-2 text-white rounded-md bg-red-500'>Subscribe 
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 mt-1">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-              </svg>
-              </button>
+              <div className='flex mt-3 space-x-1'>
+                <img src={thumbnail} alt="" className='rounded-full h-8 w-8' />
+                <p className='text-lg'>{channelTitle}</p>
+              </div>
+
+              <div className='mt-2'>
+                <button className='flex mr-10 gap-2 p-2 text-white rounded-md bg-red-500'>Subscribe
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 mt-1">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+                  </svg>
+                </button>
+              </div>
             </div>
           </div>
           <div className="w-full">
